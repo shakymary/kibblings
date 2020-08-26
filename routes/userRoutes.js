d7c57626f6c58386b465e8257535f3a7c94c7084
myhkas <mmyhkas@gmail.com>
2020-08-26 00:35:50 -0700

    added middleware for token and delete & validatetoken route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const auth = require("../middleware/auth");
 
 // register an account
 router.post("/register", async (req, res) => {
@@ -42,6 +43,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// login user
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -68,6 +70,32 @@ router.post("/login", async (req, res) => {
         email: user.email,
       },
     });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.delete("/delete", auth, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
+
+    return res.json(true);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
