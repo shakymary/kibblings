004e5ae6dd7da54fa33e1c1d5da1ec740b1c06b8
myhkas <mmyhkas@gmail.com>
2020-08-26 15:25:11 -0700

    added reactrouterdom and fix Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Btn from "./components/Button";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      {/* JumboTron Section??? */}
-      <Btn text="Delete" variant="danger"></Btn>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Btn} />
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
