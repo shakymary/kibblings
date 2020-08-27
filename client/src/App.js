da54226e59505e48927d403c3b404240a02aff37
Alvaro Centeno <alvaroc0689@gmail.com>
2020-08-26 17:25:05 -0700

    added card js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar'
 import Btn from './components/Button'
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       {/* JumboTron Section??? */}
+      <Card />
       <Btn text="Submit"
         variant="primary">
       </Btn>
