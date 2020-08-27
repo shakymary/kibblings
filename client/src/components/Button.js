7fdf60497f65d4e434aba3a95526b8e26f1e3450
Napoleon Vuong <napolo101@gmail.com>
2020-08-26 18:03:19 -0700

    Commit to merge with Mykhas code

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,18 +2,8 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 
-const Btn = (props) => {
-    return (
-        //Make Components Customizable similar in this way
-        <Button variant={props.variant}>{props.text}</Button>
-    )
-}
-
-export default Btn
-
-
 const Btn = ({ variant, text }) => {
-  return <Button variant={variant}>{text}</Button>;
+    return <Button variant={variant}>{text}</Button>;
 };
 
 export default Btn;
