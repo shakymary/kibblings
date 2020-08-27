c1d99f58f17ee10fcdabc25b35844313fa6297c6
Napoleon Vuong <napolo101@gmail.com>
2020-08-26 21:28:13 -0700

    Added form to adoption page

--- client/src/Pages/Adoption.js
+++ client/src/Pages/Adoption.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Jumbo from "../components/Jumbotron";
+import Forms from "../components/Form"
 
 const Adoption = () => {
   return (
     <div>
       <Jumbo jumbotronTitle="Hello" jumbotronText="lorem schoolboot" />
+      <Forms
+        placeholder="Search for a Pet"
+        className="mr-sm-2 secondary"
+        btnSubmitText="exampletext"
+      />
     </div>
   );
 };
