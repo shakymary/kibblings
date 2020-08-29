561e506569cc98d80ac5d8b85eafe71a9a71057d
Shakila Marando <marandoshakila1@gmail.com>
2020-08-29 12:04:20 -0700

    add footer

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,36 +4,55 @@ import Register from "../components/Auth/Register";
 import Login from "../components/Auth/Login";
 import { useHistory } from "react-router-dom";
 import UserContext from "../Context/UserContext";
-
 const NavBar = () => {
   const { userData, setUserData } = useContext(UserContext);
   const history = useHistory();
   const adoption = () => history.push("/adoption");
   const mypets = () => history.push("/mypets");
-  const home = () => history.push("/");
-
+  const home = () => history.push("/home");
+  const landing = () => history.push("/");
+  const logout = () => {
+    setUserData({
+      token: undefined,
+      user: undefined,
+    });
+    history.push("/");
+    localStorage.setItem("auth-token", "");
+  };
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand onClick={home}>Kibblings</Navbar.Brand>
+      <Navbar.Brand onClick={landing}>Kibblings</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link>
-            <Register />
-          </Nav.Link>
-          <Nav.Link>
-            <Nav.Link onClick={adoption}>Adoption</Nav.Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Nav.Link onClick={mypets}>My Pets</Nav.Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Login />
-          </Nav.Link>
+          {userData.user ? (
+            <>
+              <Nav.Link>
+                <Nav.Link onClick={home}>Home</Nav.Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Nav.Link onClick={adoption}>Adoption</Nav.Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Nav.Link onClick={mypets}>My Pets</Nav.Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Nav.Link onClick={logout}>Logout</Nav.Link>
+              </Nav.Link>
+            </>
+          ) : (
+            <>
+              <Nav.Link>
+                <Login />
+              </Nav.Link>
+              <Nav.Link>
+                <Register />
+              </Nav.Link>
+            </>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 };
-
 export default NavBar;
