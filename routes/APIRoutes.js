55ddf226e9394da674d03b82e931f76d3764b61c
Napoleon Vuong <napolo101@gmail.com>
2020-08-31 21:50:49 -0700

    API Display in Cards

--- routes/APIRoutes.js
+++ routes/APIRoutes.js
@@ -6,14 +6,13 @@ router.get("/apiToken", async (req, res) => {
         let token = {};
         await Axios.post("https://api.petfinder.com/v2/oauth2/token",
             {
-                client_id: "txUGcDgrbRsp2doKqVPp2Sepsj1SPDM0nhw9fSUF1chbfRQcar",
-                client_secret: "zwDXZ52l0IUqQQxcuzxdb7S3wtAdg6X0pQXvnogm",
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
                 grant_type: "client_credentials"
             }
         ).then((res) => {
             token.tokenType = res.data.token_type;
             token.accessToken = res.data.access_token;
-            // return { tokenType: res.data.token_type, accessToken: res.data.access_token }
         })
         res.send(token);
 
