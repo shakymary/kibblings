0e2ac27d7ce37cf7a2c20a08f98c70dddefc6d15
myhkas <mmyhkas@gmail.com>
2020-09-09 20:01:14 -0700

    pushing latest code

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,12 +5,9 @@ import Login from "../components/Auth/Login";
 import { useHistory } from "react-router-dom";
 import UserContext from "../Context/UserContext";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
-
 const NavBar = () => {
   const { userData, setUserData } = useContext(UserContext);
   const history = useHistory();
-
   const adoption = () => history.push("/adoption");
   const mypets = () => history.push("/mypets");
   const home = () => history.push("/home");
@@ -23,7 +20,6 @@ const NavBar = () => {
     history.push("/");
     localStorage.setItem("auth-token", "");
   };
-
   return (
     <Navbar bg="primary" expand="lg">
       <Navbar.Brand onClick={landing}>
@@ -34,19 +30,9 @@ const NavBar = () => {
           width="40"
           height="40"
           className="d-inline-block"
-<<<<<<< HEAD
-        />{" "}
-=======
         />{' '}
-<<<<<<< HEAD
->>>>>>> 260b62c3f95a1693586b307d5d1ca5ca01d42fc5
->>>>>>> alvaro-centeno-master
-        {/* END OF TESTING AREA */}
-=======
-        
         END OF TESTING AREA */}
         <i class="fas fa-paw"></i>
->>>>>>> 7c4ceb44c0f12eee7805c6f0a1a6c126b021747b
         Kibblings
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
