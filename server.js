b6d35ee71fec709ed98f401c756c175a6b75afd9
myhkas <mmyhkas@gmail.com>
2020-09-12 11:22:35 -0700

    fixed server

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ mongoose.connect(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // setup routes
 
 app.use("/users", require("./routes/userRoutes"));
