11747d9b2bd896da9e1c86110158e262067d3d98
Napoleon Vuong <napolo101@gmail.com>
2020-09-15 19:31:46 -0700

    Commit for Pull

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -24,7 +24,7 @@ const Landing = () => {
         <Row>
           <Col style={{ paddingTop: "60px" }}>
             <h1 style={{ fontSize: "105px", textAlign: "center" }}>About Kibblings!</h1>
-            <h1 style={{ fontSize: "105px", textAlign: "center" }}>🐱🐶🧫🐰🐹</h1>
+            <h1 style={{ fontSize: "105px", textAlign: "center" }}>🐱🐶🐴🐰🐹</h1>
             <h5 className="mb-2 text-muted" style={{ textAlign: "center" }}>Created by Mykhas Nallas, Napoleon Vuong, Alvaro Centeno, and Shakila
           Marando</h5>
           </Col>
