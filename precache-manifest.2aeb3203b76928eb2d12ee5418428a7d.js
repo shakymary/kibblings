c6529bfc9e372d2569d48a37d5d27c8fedd05464
Shakila Marando <marandoshakila1@gmail.com>
2020-09-17 16:30:05 -0700

    Updates

---
+++ precache-manifest.2aeb3203b76928eb2d12ee5418428a7d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e3606cd5a9d98fc32cfe501e16cd67",
+    "url": "/kibblings/index.html"
+  },
+  {
+    "revision": "6516279edd25a501d439",
+    "url": "/kibblings/static/css/2.eae59cc8.chunk.css"
+  },
+  {
+    "revision": "34d6875ecb45183e1d96",
+    "url": "/kibblings/static/css/main.b569c5b3.chunk.css"
+  },
+  {
+    "revision": "6516279edd25a501d439",
+    "url": "/kibblings/static/js/2.e5d2c0b5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/kibblings/static/js/2.e5d2c0b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34d6875ecb45183e1d96",
+    "url": "/kibblings/static/js/main.437a757f.chunk.js"
+  },
+  {
+    "revision": "0c594c2dd8e87bd4861e",
+    "url": "/kibblings/static/js/runtime-main.cc4f7d0e.js"
+  },
+  {
+    "revision": "38c4cf109fc1d4912d47838cd7420923",
+    "url": "/kibblings/static/media/CatLanding2.38c4cf10.mp4"
+  },
+  {
+    "revision": "2cbb6439a4c0192fb86b4d9edcbb267a",
+    "url": "/kibblings/static/media/FrenchBulldogLanding2.2cbb6439.mp4"
+  },
+  {
+    "revision": "6e9dc3a4979622a07cf3308ea7087985",
+    "url": "/kibblings/static/media/LandingAdoption2.6e9dc3a4.mp4"
+  },
+  {
+    "revision": "51a527729f6a4e97a0ca2ada9ed122ca",
+    "url": "/kibblings/static/media/PlaceHolder1920x1080.51a52772.mp4"
+  },
+  {
+    "revision": "de63c04e18633762b973bebbdc89e8b3",
+    "url": "/kibblings/static/media/about_pageImage.de63c04e.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/kibblings/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/kibblings/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/kibblings/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/kibblings/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/kibblings/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/kibblings/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/kibblings/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/kibblings/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/kibblings/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/kibblings/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/kibblings/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/kibblings/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/kibblings/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/kibblings/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/kibblings/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/kibblings/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/kibblings/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
